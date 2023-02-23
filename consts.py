f9117c91b4250364721bac8d4c12344984ab894b
npuchkin <npuchkin@gmail.com>
2023-02-23 21:52:47 +0300

    Add files via upload

---
+++ consts.py
@@ -0,0 +1,58 @@
+threshold_p_2_f = 2.4917
+threshold_p_2_poly = 2.4594
+threshold_nn = 4.6886
+
+threshold_kliep_18 = 6.8485
+threshold_kliep_20 = 6.0889
+threshold_kliep_25 = 5.3147
+threshold_kliep_30 = 4.6608
+threshold_kliep_33 = 4.1893
+threshold_kliep_35 = 3.8868
+threshold_kliep_40 = 3.2149
+threshold_kliep_50 = 2.2427
+
+threshold_mmd_10 = 36.745
+threshold_mmd_15 = 42.9836
+threshold_mmd_20 = 34.0885
+threshold_mmd_25 = 28.4776
+threshold_mmd_30 = 22.713
+threshold_mmd_35 = 17.9604
+threshold_mmd_45 = 11.6102
+threshold_mmd_50  = 9.5924
+
+threshold_cusum = 0.4469
+threshold_cusum_squared = 0.1465
+
+threshold_p_3_poly = 3.9832
+threshold_p_3_f = 3.9518
+
+threshold_p_6_poly = 4.185
+threshold_p_6_f = 6.4894
+
+threshold_nn_1 = 0.103
+threshold_p_10_f_1 = 2.4887
+threshold_p_10_poly_1 = 0.0
+
+threshold_nn_2 = 4.0415
+threshold_p_10_f_2 = 4.8811
+threshold_p_10_poly_2 = 3.1413
+
+threshold_nn_3 = 4.2503
+threshold_p_10_f_3 = 4.8741
+threshold_p_10_poly_3 = 3.1408
+
+threshold_nn_4 = 4.9224
+threshold_p_10_f_4 = 6.6555
+threshold_p_10_poly_4 = 3.9662
+
+threshold_kliep_1 = 3.8682 
+threshold_mmd_1 = 10.2544
+
+threshold_kliep_2 = 3.8682 
+threshold_mmd_2 = 10.2544
+
+threshold_kliep_3 = 3.8682 
+threshold_mmd_3 = 10.2544
+
+threshold_kliep_4 = 3.8682 
+threshold_mmd_4 = 10.2544
---
+++ readme.txt
@@ -0,0 +1,9 @@
+
+Numerical experiments, described in the paper "A Contrastive Approach to Online Change Point Detection" by N. Puchkin and V. Shcherbakova (arXiv:2206.10143).
+
+
+1) The file 'online_change_point_detection.ipynb' contains the code of the procedure and baselines and the numerical experiments on artificial and CENSREC-1-C data sets with computation of the detection delay. The detection delays are written in the files in the 'DD' folder. The plots are saved to the 'images' folder.
+
+2) The file 'thresholds.ipynb' contains the code with computation of the thresholds of the procedures for artificial experiments. These thresholds are saved in the 'thresholds' folder and then used in 'online_change_point_detection.ipynb'.
+
+3) The file 'WISDM_experiments.ipynb' contains the code of the procedure and baselines and the numerical experiments on the WISDM data set with computation of the detection delay. The plots are saved to the 'images' folder.
